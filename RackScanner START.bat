9561d77b28c6dd44d74bb2047328db87370843cb
Michal Kahle <michalkahle@gmail.com>
2020-05-21 01:10:32 +0200

    Update RackScanner START.bat

--- RackScanner START.bat
+++ RackScanner START.bat
@@ -1,2 +1,2 @@
 start http://localhost:8000
-cmd /k "%userprofile%\Miniconda2\Scripts\activate.bat RackScanner & python http_server.py"
+cmd /k "%userprofile%\Miniconda3\Scripts\activate.bat RackScanner & python http_server.py"
